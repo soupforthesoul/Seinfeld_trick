46023ab2b6a1b65d6d4c76ca3590a4a5be40828a
Shrimant Jaruhar <s.jaruhar@gmail.com>
2013-01-27 18:16:00 -0600

    my first commit

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 $(document).ready(function(){
 	
+//	$(document).login();
+
+	/*
+
+	 $('html').click(function(){             
+	 var currentDate = $( "#datepicker" ).datepicker( "getDate" );
+
+	 $('#dummy').append('\n<div class="green">'+currentDate+"</div>");
+});
+
+
+		if($(this).css("background-color")==="rgb(0, 128, 0)")
+			{$(this).css("background-color","rgb(0, 50, 0)");}
+			else
+			{$(this).css("background-color","green");}
+
+
 	for(var x=1;x<=31;x++)
-	{$('#dummy').append('<div>'+x+"</div>");}
+	{$('#dummy').append('<div class="green">'+x+"</div>");}
 	
 	
 	$('div').click(function(){
@@ -11,4 +28,6 @@ $(document).ready(function(){
 			else
 			{$(this).css("background-color","green");}
 		});
+
+*/
 });
